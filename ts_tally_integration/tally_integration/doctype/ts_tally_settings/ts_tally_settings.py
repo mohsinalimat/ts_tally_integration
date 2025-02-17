daee86e3cff51a750fdb0433033a8c2cd1923ac8
Abinandhan-TS <abinandhans@thirvusoft.in>
2025-02-17 18:20:12 +0530

    chore: Code Changes for TS tally Settings

--- ts_tally_integration/tally_integration/doctype/ts_tally_settings/ts_tally_settings.py
+++ ts_tally_integration/tally_integration/doctype/ts_tally_settings/ts_tally_settings.py
@@ -1,9 +1,63 @@
 # Copyright (c) 2024, Siddarth and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.core.doctype.user.user import generate_keys
+from frappe.utils import get_url
+from frappe.permissions import add_permission
 
 
 class TSTallySettings(Document):
 	pass
+
+
+def user_creation(user_id):
+
+    if frappe.db.exists("User",user_id):
+        return
+    user = frappe.new_doc("User")
+    user.email = user_id
+    user.first_name = "Tally"
+    user.last_name = "User"
+    user.send_welcome_email = 0
+    user.username = "tallyuser"
+    user.add_roles("Tally User")
+    user.save()
+    api_generate_secret = generate_keys(user_id)
+
+    secret_key = api_generate_secret["api_secret"]
+    api_key = frappe.db.get_value("User", user_id, "api_key")
+    
+    base_url = get_url()
+    purchase_invoice_inventory = f"{base_url}/api/method/ts_tally_integration.tally_integration.utils.api.purchase_invoice_inventory.get_purchase_invoice"
+    purchase_invoice_non_inventory = f"{base_url}/api/method/ts_tally_integration.tally_integration.utils.api.purchase_invoice_non_inventory.get_purchase_invoice"
+    debit_note_inventory = f"{base_url}/api/method/ts_tally_integration.tally_integration.utils.api.debit_note_inventory.get_debit_note"
+    debit_note_non_inventory = f"{base_url}/api/method/ts_tally_integration.tally_integration.utils.api.debit_note_non_inventory.get_debit_note"
+    payment_entry_pay = f"{base_url}/api/method/ts_tally_integration.tally_integration.utils.api.payment_entry_pay.get_payment_entry"
+    payment_entry_receipt = f"{base_url}/api/method/ts_tally_integration.tally_integration.utils.api.payment_entry_receipt.get_payment_entry"
+
+    api_details = f'''API Key: {api_key}\n\nSecret Key: {secret_key}\n\nPurchase Invoice(Inventory): {purchase_invoice_inventory}\n\nPurchase Invoice(Non-Inventory): {purchase_invoice_non_inventory}\n\nDebit Note(Inventory): {debit_note_inventory}\n\nDebit Note(Non-Inventory): {debit_note_non_inventory}\n\nPayment Entry(Pay): {payment_entry_pay}\n\nPayment Entry(Receipt): {payment_entry_receipt}'''
+    
+    frappe.db.set_value("TS Tally Settings", "TS Tally Settings", "api_details", api_details)
+
+def role_creation(role_name):
+    if frappe.db.exists("Role", role_name):
+        return
+    role = frappe.new_doc("Role")
+    role.role_name = role_name
+    role.desk_access = 0
+    role.save()
+    
+
+def role_permission(role_name):
+    doctypes = ["Sales Invoice", "Purchase Invoice", "Delivery Note", "Purchase Receipt", "Stock Entry", "Payment Entry"]
+    
+    for doctype in doctypes:
+        existing_permission = frappe.db.exists("Custom DocPerm", {"parent": doctype, "role": role_name})
+        
+        if not existing_permission:
+            add_permission(doctype, role_name, 0, "read")
+           
+
+   
--- ts_tally_integration/tally_integration/utils/py/after_install.py
+++ ts_tally_integration/tally_integration/utils/py/after_install.py
@@ -1,7 +1,15 @@
-from ts_tally_integration.tally_integration.utils.py.user import user_creation
+from ts_tally_integration.tally_integration.doctype.ts_tally_settings.ts_tally_settings import user_creation, role_creation, role_permission
 
 
 def after_install():
+    user_id = "tally@thirvusoft.co.in"
+    role_name = "Tally User"
+    print("Creating Role...")
+    role_creation(role_name)
+    print("Role Created")
+    print("Creating Permissions...")
+    role_permission(role_name)
+    print("Permissions Created")
     print("Creating Tally User...")
-    user_creation()
+    user_creation(user_id)
     print("Tally User Created")
