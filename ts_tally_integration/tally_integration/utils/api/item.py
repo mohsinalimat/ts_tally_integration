74212183a3583fb3c771995ee47b3a3c17295777
siddarth-thirvusoft <siddarthkanagaraj@thirvusoft.co.in>
2025-12-23 15:51:08 +0530

    fix: sending item code instream of item name, removed item limit

--- ts_tally_integration/tally_integration/utils/api/item.py
+++ ts_tally_integration/tally_integration/utils/api/item.py
@@ -95,7 +95,7 @@ def fetch_response(response=None):
         if not item_name:
             continue
         
-        item_docname = frappe.db.get_value("Item", {"name": item_name}, "name")
+        item_docname = frappe.db.get_value("Item", {"item_name": item_name}, "name")
 
         if item_docname:
             frappe.db.set_value(
